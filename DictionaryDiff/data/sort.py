b0ba73962826c0100bef156aa73c50d8cd61f760
Jaume Adrover Fernández <jaumeadroverfernandez@gmail.com>
2023-05-17 07:36:49 +0200

    Added metadata and sorted dicts

--- DictionaryDiff/data/sort.py
+++ DictionaryDiff/data/sort.py
@@ -1,16 +1,15 @@
+dicts=["CAT.txt","DEN.txt","FRA.txt","DEU.txt","ITA.txt","NOR.txt","POR.txt","ESP.txt","SWE.txt","ENG.txt"]
+dicts2=["CAT_sorted.txt","DEN_sorted.txt","FRA_sorted.txt","DEU_sorted.txt","ITA_sorted.txt","NOR_sorted.txt","POR_sorted.txt","ESP_sorted.txt","SWE_sorted.txt","ENG_sorted.txt"]
 
-dicts=["CAT.txt","DEN.txt","FRA.txt","DEU.txt","ITA.txt","NOR.txt","POR.txt","ESP.txt","SWE.txt",]
-dicts2=["CAT_sorted.txt","DEN_sorted.txt","FRA_sorted.txt","DEU_sorted.txt","ITA_sorted.txt","NOR_sorted.txt","POR_sorted.txt","ESP_sorted.txt","SWE_sorted.txt"]
 
-
+#Bucle per a convertir tot a minúscules i ordenar per longitud
 for dict in dicts:
-    with open(dict,encoding="utf8") as f:
+    with open("Raw/"+dict,encoding="utf8") as f:
         words= [line.strip().lower() for line in f]
+    #Ordenam per longitud
     sorted_words = sorted(words, key=lambda w: len(w))
-    # Extract the substring before the '.' character using string slicing
-    substring = dict[:dict.index('.')]
-    substring=substring+str("_sorted.txt")
-    print(substring)
+    substring = dict[:dict.index('.')] #Extreim nom del fitxer
+    substring=substring+str("_sorted.txt") #Cream nou fitxer optimitzat
     with open(substring,'w',encoding="utf8") as f2:
         for word in sorted_words:
             f2.write(word+'\n')
@@ -18,26 +17,24 @@ for dict in dicts:
 
 # Open output file for writing
 for dict in dicts2:
-    indexos=[0]
+    indexos={}
     idx=1
     pos=0
     with open(dict, 'r+',encoding="utf8") as f:
         line=f.readline()
         while line:
             lon=len(line)-1
-            #print("Paraula: "+str(line))
-            #print("Longitud: "+str(lon))
             if lon==idx:
                 pos=pos+1
             else:#canviam longitud
                 idx=idx+1
-                #print("Paraula: "+str(line))
-                #print("Longitud: "+str(lon))
-                #print(idx)
-                indexos.append(pos)
+                indexos[idx]=pos
                 pos=pos+1
             line=f.readline()
         print(indexos)
-    # Write each word to file on a new line
- #   for word in sorted_words:
-  #      f.write(word + '\n')
+        substring=dict[:dict.index('_')]+"_metadata.txt"
+        with open(substring,'w',encoding="utf8") as f2:
+            f2.seek(0) #Ubicam al punter a l'inici del fitxer
+            for key,value in indexos.items():
+                f2.write(str(key)+","+str(value)+'\n')
+
--- DictionaryDiff/src/Model/Idioma.java
+++ DictionaryDiff/src/Model/Idioma.java
@@ -1,52 +1,92 @@
 package Model;
+
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.io.IOException;
 import java.nio.file.Paths;
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
+
 public class Idioma {
-    List<String> words;
+    private List<String> words;
 
-    String filename;
+    private List<String> sortedWords;
 
-    //String filesorted;
+    private int indexos[];
+
 
     String nom;
 
     public Idioma( String nom) throws FileNotFoundException {
         String currentDir = Paths.get("").toAbsolutePath().toString();
-        this.filename=currentDir +"/data/"+nom+".txt";
         this.nom = nom;
+
+        //Llegim paraules originals
+        String filename=currentDir +"/data/Raw/"+nom+".txt";
+        initWords(filename);
+
+        //Obtenim paraules ordenades
+        filename=currentDir+"/data/Sorted/"+nom+"_sorted.txt";
+        initSortedWords(filename);
+
+        //Obtenim meta-informació
+        filename=currentDir+"/data/Metadata/"+nom+"_metadata.txt";
+        initMetadata(filename);
+    }
+
+    private void initWords(String filename){
         words = new ArrayList<>();
 
         try (BufferedReader br = new BufferedReader(new FileReader(filename))) {
             String line;
             while ((line = br.readLine()) != null) {
-                System.out.println("LINE: "+line);
                 words.add(line);
             }
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException(e);
-        } catch (IOException e) {
+        } catch (Exception e) {
             throw new RuntimeException(e);
         }
-        System.out.println();
+    }
+    private void initSortedWords(String filename){
+        sortedWords = new ArrayList<>();
+
+        try (BufferedReader br = new BufferedReader(new FileReader(filename))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                sortedWords.add(line);
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
-    public void ordena(){
-        // Ordenar el ArrayList por longitud de cadenas
-        Collections.sort(words, new Comparator<String>() {
-            @Override
-            public int compare(String s1, String s2) {
-                return Integer.compare(s1.length(), s2.length());
+    private void initMetadata(String filename){
+        indexos=new int[15];
+        String numbers[];
+
+        //Inicialitzam array nombres
+        for (int i = 0; i < indexos.length; i++) {
+            indexos[i]=-1;
+        }
+        indexos[1]=0; //les paraules amb len 1 comencen desde 0
+
+
+        try (BufferedReader br = new BufferedReader(new FileReader(filename))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                numbers=line.split(",");
+                int longitud=Integer.parseInt(numbers[0]);
+                int index=Integer.parseInt(numbers[1]);
+                indexos[longitud]=index;
             }
-        });
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
     public String[] getWords(){
         return words.toArray(new String[0]);
+    }
+
+    public String[] getSortedWords(){
+        return sortedWords.toArray(new String[0]);
     }
 }
